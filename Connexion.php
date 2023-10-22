327be92c7f298c625a6ea5ea35e6ec44dbc70672
clemse32 <117434193+clemse32@users.noreply.github.com>
2023-10-22 10:52:16 +0200

    Connexion base de donnée1

--- Connexion.php
+++ Connexion.php
@@ -1,9 +1,44 @@
-<?php
-    try{
-        $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        $bdd = new PDO ('mysql:host=localhost;dbname=ceramique', 'root','',$pdo_options);
-        echo "ça marche";
-        } catch(Exception $ex) {
-        die("Erreur: ".$ex->getMessage());
-        }
-?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css" />
+    <title>Annie B. Céramique - Connexion</title>
+</head>
+
+<body>
+
+    <?php
+    include "./Asset/Header.HTML";
+    ?>
+
+    <?php
+    include "./Asset/nav.HTML";
+    ?>
+
+    <section>
+        <h2>Administration</h2>
+
+        <form action="login.php" method="post">
+            <label for="login">Nom d'utilisateur:</label>
+            <input type="text" id="login" name="login" required>
+
+            <label for="password">Mot de passe:</label>
+            <input type="password" id="password" name="password" required>
+
+            <button type="submit">Se connecter</button>
+        </form>
+
+    </section>
+
+    <footer>
+        <?php
+        include "Asset/Footer.HTML";
+        ?>
+    </footer>
+
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+try {
+    $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+    $bdd = new PDO('mysql:host=localhost;dbname=ceramique', 'Annie BURETTE', 'admin', $pdo_options);
+
+    // Récupérez les données du formulaire
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+
+    // Recherche de l'utilisateur en fonction du nom d'utilisateur
+    $query = $bdd->prepare("SELECT * FROM user WHERE login = :login");
+    $query->execute(array('login' => $login));
+    $user = $query->fetch();
+
+    if ($user && password_verify($password, $user['password'])) {
+        // Authentification réussie
+        // Rediriger vers la page d'accueil (index.php)
+        header("Location: index.php");
+        exit();
+    } else {
+        // Authentification échouée
+        echo "Nom d'utilisateur ou mot de passe incorrect.";
+        // Vous pouvez ajouter un lien pour revenir à la page de connexion.
+    }
+} catch (Exception $ex) {
+    die("Erreur: " . $ex->getMessage());
+}
+?>
